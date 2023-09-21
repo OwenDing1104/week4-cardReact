24d3c160adbcc396cd4ae64b3eb0d4ebbe1ebe57
OwenDing1104 <karsaowen@gmail.com>
2023-09-21 12:47:58 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <h1>Welcome to my card</h1>
-        <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
-        <div className="paragraph">
-          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
+      <div className="card">
+        <h1>NBA Superstar</h1>
+        <img src="https://thumbs.dreamstime.com/b/michael-jordan-chicago-bulls-superstar-image-taken-color-slide-73861883.jpg" alt="Jordan" />
+        <div className="content">
+          <h2><b>Michael Jordan</b></h2>
+            <p>Michael Jeffrey Jordan (born February 17, 1963), also known by his initials MJ, is an American former professional basketball player and businessman. Widely considered the greatest basketball player of all time, the official National Basketball Association (NBA) website states that "by acclamation, Michael Jordan is the greatest basketball player of all time." He played fifteen seasons in the NBA, winning six NBA championships with the Chicago Bulls. He was integral in popularizing the sport of basketball and the NBA around the world in the 1980s and 1990s, becoming a global cultural icon.</p>
+        <div className="button1">
+          <button id="detail1">Details</button>
+          <button id="cloneButton1">CloneCard</button>
+          <button id="color1">ColorCard</button>
+          <button id="heading1">ChangeHeading</button>
+          <button id="delete1">Delete</button>  
+        </div> 
         </div>
-        <div class="btn-wrapper">
- <a href="https://hax.psu.edu">
-   <button>Details</button> </a> </div>
       </div>
     );
-  }
+}
 
 export default App;
